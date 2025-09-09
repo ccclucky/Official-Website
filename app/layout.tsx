b8dd1117605c3b4ddfd095561d3150e07080421a
cclucky <2226114701@qq.com>
2025-09-09 12:50:29 +0800

    fix:修改静态资源

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ import { seo } from '~/lib/seo'
 export const metadata: Metadata = {
   metadataBase: seo.url,
   title: {
-    template: '%s | CC Blog',
+    template: '%s | CCLUCKY BLOG',
     default: seo.title,
   },
   description: seo.description,
@@ -34,10 +34,10 @@ export const metadata: Metadata = {
   openGraph: {
     title: {
       default: seo.title,
-      template: '%s | CC Blog',
+      template: '%s | CCLUCKY BLOG',
     },
     description: seo.description,
-    siteName: 'CC Blog',
+    siteName: 'CCLUCKY BLOG',
     locale: 'zh_CN',
     type: 'website',
     url: 'https://www.cclove.eu.org',
--- lib/seo.ts
+++ lib/seo.ts
@@ -1,5 +1,5 @@
 export const seo = {
-  title: 'CC Blog | 开发者、思考者、探索者、幸运的人',
+  title: 'CCLUCKY BLOG | 开发者、思考者、探索者、幸运的人',
   description:
     '我叫 cclucky，一名开发者，思考者，探索者，同时也是MetaBubble实验室的成员，目前带领着MetaBubble实验室致力于创造一个充满创造力的工作环境，同时鼓励团队创造影响世界的产品。',
   url: new URL(
