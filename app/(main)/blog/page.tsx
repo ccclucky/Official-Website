b5028e007ed1ab97ba0262267c2a0380f50452e5
cclucky <2226114701@qq.com>
2025-09-12 17:54:24 +0800

    fix:恢复社交组件

--- app/(main)/blog/page.tsx
+++ app/(main)/blog/page.tsx
@@ -32,9 +32,10 @@ export default function BlogPage() {
         <p className="my-6 text-base text-zinc-600 dark:text-zinc-400">
           <Balancer>{description}</Balancer>
         </p>
-        {/*<p className="flex items-center">*/}
-        {/*  <SocialLink href="/feed.xml" platform="rss" />*/}
-        {/*</p>*/}
+        <p className="flex items-center">
+          <SocialLink href="https://github.com/ccclucky" platform="github" />
+          <SocialLink href="mailto:jingc5946@gmail.com" platform="mail" />
+        </p>
       </header>
       <div className="mt-12 grid grid-cols-1 gap-6 sm:mt-20 lg:grid-cols-2 lg:gap-8">
         <BlogPosts limit={20} />
